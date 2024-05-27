67375ed0815843ca13e59955022a90f53b9486cc
Kaido Vetevoog <kaido.vetevoog@gmail.com>
2024-05-27 16:06:37 +0300

    test fix

--- tests/agregator/test_agregator.py
+++ tests/agregator/test_agregator.py
@@ -5,10 +5,5 @@ class TestAgregator():
     """Tests for entries agregator class."""
 
     def test_init_agregator_entries(self):
-        ag = AgregatorEntries()
-        assert len(ag.entries) == 0
-
-    def test_init_agregator_entries_from_file(self):
-        ag = AgregatorEntries()
-        ag.load_entries_from_file("annual_report/tests/entry/one_entry.json")
+        ag = AgregatorEntries("annual_report/tests/entry/one_entry.json")
         assert len(ag.entries) > 0
