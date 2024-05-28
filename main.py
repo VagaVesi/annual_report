1064c3ce69b6339d4b0e132c7d8539c24f06c760
Kaido Vetevoog <kaido.vetevoog@gmail.com>
2024-05-28 13:58:51 +0300

    Additional subaccount dimensions

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 """Call all functions to simulatete annual accounts process."""
 
-# from path import Path
-# from json import loads
-# from report_data.report_data import ReportData, generate_report_element_filtering_rules, save_as_json
+from path import Path
+from json import loads
 # from tools.json_validator import validate
 # from classifications.classification import ClassificationsList
+from report_data.report_data import ReportData, generate_report_element_filtering_rules, save_as_json
 from agregator.agregator import save_ledger, generate_ledger_from_entries
-# from classifications.classification import ClassificationsList
-# from annual_report.report_data.pattern import Pattern
+from report_data.pattern import Pattern
 
 
 def main():
@@ -23,16 +22,16 @@ def main():
 
     """Generate report elements from dataset"""
     # mapping1 = generate_report_element_filtering_rules()
-    # save_as_json(mapping1, "report_element_accounts_filter_rules")
+    # save_as_json(mapping1, "report_element_filter_rules")
 
-    """Generate from transactions file a dataset file."""
+    """Generate from transactions file a ledger file."""
     # sample_ledger_micro = generate_ledger_from_entries("annual_report/tests/entry/modified_entries_list.json", ["EE0301010"],
     #                                                    "11308014", "2023-01-01", "2023-12-31")
-    # save_ledger(sample_ledger_micro)
+    # # save_ledger(sample_ledger_micro)
 
-    sample_ledger_standard = generate_ledger_from_entries("annual_report/tests/entry/modified_entries_list.json", ["EE0301020",  "EE0302010"],
-                                                          "13333333", "2023-01-01", "2023-12-31")
-    save_ledger(sample_ledger_standard)
+    # sample_ledger_standard = generate_ledger_from_entries("annual_report/tests/entry/modified_entries_list.json", ["EE0301020",  "EE0302010"],
+    #                                                       "13333333", "2023-01-01", "2023-12-31")
+    # save_ledger(sample_ledger_standard)
 
     """Validate json file."""
     # file_name = "annual_report/agregator/output/11308014-2024-05-13T14:14:07:726.json"
@@ -43,16 +42,15 @@ def main():
     # result = validate(dataset, schema)
     # print(result)
 
-    """Generate report elements for micro"""
-    # path = Path(
-    #     "annual_report/tests/report_data/source_data/sample_dataset_micro.json")
-    # sample_dataset = loads(path.read_text(encoding="utf-8"))
-    # report_data = ReportData(sample_dataset)
-    # save_as_json(report_data.account_combinations_mapping,
-    #              "account_combination_report_elements_mapping")
-
-    # report_elements_for_xbrl = report_data.return_report_elements()
-    # print(report_elements_for_xbrl)
+    """Generate report elements"""
+    path = Path(
+        "annual_report/agregator/output/13333333-2024-05-28T13:29:42:578.json")
+    sample_ledger = loads(path.read_text(encoding="utf-8"))
+    report_data = ReportData(sample_ledger)
+    mapping2 = report_data.generate_account_combination_report_elements_mapping_rules()
+    save_as_json(mapping2, "account_combination_elements_mapping")
+    report_elements_for_xbrl = report_data.return_report_elements()
+    save_as_json(report_elements_for_xbrl, "standard_report_data_for_xbrl")
 
 
 if __name__ == "__main__":
